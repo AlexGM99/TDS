f92a6b3f61a9b8b1b681bc8238229d773bf8216a
EduPeMa <edupema98@gmail.com>
2019-10-22 10:43:18 +0200

    commit inicial edu

---
+++ AppChat/src/main/java/AlexAndEDU/SLA/AppChat/App.java
@@ -0,0 +1,13 @@
+package AlexAndEDU.SLA.AppChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
