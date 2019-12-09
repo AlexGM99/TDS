4f7f44a951b5b2dc543369950be4a4f7f82f783f
AlexGM99 <alejandro.g.mt99@gmail.com>
2019-12-09 12:48:21 +0100

    Gráfica de registro terminada

--- APPCHAT/src/main/java/interfazGrafica/Register.java
+++ APPCHAT/src/main/java/interfazGrafica/Register.java
@@ -3,10 +3,33 @@ package interfazGrafica;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.Dimension;
 
 public class Register {
 
-	private JFrame frame;
+	private JFrame frmAppchatregister;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_7;
+	private JPasswordField passwordField;
+	private JCheckBox chckbxPremium;
+	private JButton btnRegister;
+	private JButton btnIveAnAccount;
 
 	/**
 	 * Launch the application.
@@ -16,7 +39,7 @@ public class Register {
 			public void run() {
 				try {
 					Register window = new Register();
-					window.frame.setVisible(true);
+					window.frmAppchatregister.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +58,173 @@ public class Register {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAppchatregister = new JFrame();
+		frmAppchatregister.setMinimumSize(new Dimension(600, 350));
+		frmAppchatregister.setTitle("APPCHAT-Register");
+		frmAppchatregister.setBounds(100, 100, 450, 300);
+		frmAppchatregister.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{150, 0, 122, 0, 150, 0};
+		gridBagLayout.rowHeights = new int[]{50, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frmAppchatregister.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel = new JLabel("first name");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		frmAppchatregister.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		frmAppchatregister.getContentPane().add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("second name");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 1;
+		gbc_lblNewLabel_4.gridy = 2;
+		frmAppchatregister.getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 2;
+		frmAppchatregister.getContentPane().add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("birthday");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 3;
+		frmAppchatregister.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 3;
+		frmAppchatregister.getContentPane().add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		chckbxPremium = new JCheckBox("premium");
+		GridBagConstraints gbc_chckbxPremium = new GridBagConstraints();
+		gbc_chckbxPremium.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxPremium.gridx = 3;
+		gbc_chckbxPremium.gridy = 3;
+		frmAppchatregister.getContentPane().add(chckbxPremium, gbc_chckbxPremium);
+		
+		JLabel lblNewLabel_2 = new JLabel("email");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 4;
+		frmAppchatregister.getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 2;
+		gbc_textField_3.gridy = 4;
+		frmAppchatregister.getContentPane().add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("phone");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 1;
+		gbc_lblNewLabel_3.gridy = 5;
+		frmAppchatregister.getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 2;
+		gbc_textField_4.gridy = 5;
+		frmAppchatregister.getContentPane().add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNick = new JLabel("nick");
+		GridBagConstraints gbc_lblNick = new GridBagConstraints();
+		gbc_lblNick.anchor = GridBagConstraints.WEST;
+		gbc_lblNick.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNick.gridx = 1;
+		gbc_lblNick.gridy = 6;
+		frmAppchatregister.getContentPane().add(lblNick, gbc_lblNick);
+		
+		textField_5 = new JTextField();
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 2;
+		gbc_textField_5.gridy = 6;
+		frmAppchatregister.getContentPane().add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblPass = new JLabel("pass");
+		GridBagConstraints gbc_lblPass = new GridBagConstraints();
+		gbc_lblPass.anchor = GridBagConstraints.WEST;
+		gbc_lblPass.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPass.gridx = 1;
+		gbc_lblPass.gridy = 7;
+		frmAppchatregister.getContentPane().add(lblPass, gbc_lblPass);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 2;
+		gbc_passwordField.gridy = 7;
+		frmAppchatregister.getContentPane().add(passwordField, gbc_passwordField);
+		
+		JLabel lblAvatar = new JLabel("avatar");
+		GridBagConstraints gbc_lblAvatar = new GridBagConstraints();
+		gbc_lblAvatar.anchor = GridBagConstraints.WEST;
+		gbc_lblAvatar.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAvatar.gridx = 1;
+		gbc_lblAvatar.gridy = 8;
+		frmAppchatregister.getContentPane().add(lblAvatar, gbc_lblAvatar);
+		
+		textField_7 = new JTextField();
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_7.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_7.gridx = 2;
+		gbc_textField_7.gridy = 8;
+		frmAppchatregister.getContentPane().add(textField_7, gbc_textField_7);
+		textField_7.setColumns(10);
+		
+		btnRegister = new JButton("register");
+		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
+		gbc_btnRegister.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRegister.gridx = 2;
+		gbc_btnRegister.gridy = 9;
+		frmAppchatregister.getContentPane().add(btnRegister, gbc_btnRegister);
+		
+		btnIveAnAccount = new JButton("I've an account");
+		GridBagConstraints gbc_btnIveAnAccount = new GridBagConstraints();
+		gbc_btnIveAnAccount.insets = new Insets(0, 0, 5, 5);
+		gbc_btnIveAnAccount.gridx = 2;
+		gbc_btnIveAnAccount.gridy = 10;
+		frmAppchatregister.getContentPane().add(btnIveAnAccount, gbc_btnIveAnAccount);
 	}
 
 }
